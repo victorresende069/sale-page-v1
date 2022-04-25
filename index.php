1407de3802d4eaaef3f8facd7dfa6b42f183e80e
Victor Resende <xrez.devops@gmail.com>
2022-04-25 16:19:54 -0300

    Update

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina de Venda - {PRODUTO}</title>
+    <link rel="stylesheet" href="./assets/css/page.css" />
+</head>
+
+<body>
+
+    <!--CONTAINER-->
+    <div id="container">
+        <header>
+            <div class="hdr">
+                <div class="logo"><img src="./assets/images/logo.png" /></div>
+                <div class="sub-logo">
+                    <h4>Pagina de Venda - {PRODUTO}</h4>
+                </div>
+            </div>
+        </header>
+
+        <section id="products">
+            <div class="textProduct">
+                <h1>OS MELHORES PRODUTO</h1>
+                <h2>Com desconto exclusivos</h2>
+            </div>
+            <div class="list_product">
+
+                <div class="product_client1">
+                    <div class="price_product">
+                        <h2>R$ 250.00</h2>
+                    </div>
+                    <div class="img_product"><img src="./assets/images/product/product1.webp" /></div>
+                    <div class="title_product">
+                        <h1>Tênis Nike</h1>
+                    </div>
+                    <div class="desc_product">
+                        <p>Modelo Flash Point, Acabamento revestido de azul e bordas de ouro</p>
+                    </div>
+                    <div class="buy_product">
+                        <a type="button" href="https://www.google.com/tenisnike">COMPRAR</a>
+                    </div>
+                </div>
+
+
+
+                <div class="product_client1">
+                    <div class="price_product">
+                        <h2>R$ 250.00</h2>
+                    </div>
+                    <div class="img_product"><img src="./assets/images/product/product1.webp" /></div>
+                    <div class="title_product">
+                        <h1>Tênis Nike</h1>
+                    </div>
+                    <div class="desc_product">
+                        <p>Modelo Flash Point, Acabamento revestido de azul e bordas de ouro</p>
+                    </div>
+                    <div class="buy_product">
+                        <a type="button" href="https://www.google.com/tenisnike">COMPRAR</a>
+                    </div>
+                </div>
+                <div class="product_client1">
+                    <div class="price_product">
+                        <h2>R$ 250.00</h2>
+                    </div>
+                    <div class="img_product"><img src="./assets/images/product/product1.webp" /></div>
+                    <div class="title_product">
+                        <h1>Tênis Nike</h1>
+                    </div>
+                    <div class="desc_product">
+                        <p>Modelo Flash Point, Acabamento revestido de azul e bordas de ouro</p>
+                    </div>
+                    <div class="buy_product">
+                        <a type="button" href="https://www.google.com/tenisnike">COMPRAR</a>
+                    </div>
+                </div>
+
+                <div class="product_client1">
+                    <div class="price_product">
+                        <h2>R$ 250.00</h2>
+                    </div>
+                    <div class="img_product"><img src="./assets/images/product/product1.webp" /></div>
+                    <div class="title_product">
+                        <h1>Tênis Nike</h1>
+                    </div>
+                    <div class="desc_product">
+                        <p>Modelo Flash Point, Acabamento revestido de azul e bordas de ouro</p>
+                    </div>
+                    <div class="buy_product">
+                        <a type="button" href="https://www.google.com/tenisnike">COMPRAR</a>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+
+
+
+
+    </div>
+    <!--CONTAINER-->
+
+    <script type="text/javascript" src="./assets/js/page.js"></script>
+</body>
+
+</html>
