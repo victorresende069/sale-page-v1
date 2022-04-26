6294fd4eb83276b387fb317dc1a6005ebbdd2672
Victor Resende <xrez.devops@gmail.com>
2022-04-26 14:53:33 -0300

    update

--- index.php
+++ index.php
@@ -88,7 +88,7 @@
                         <p>Modelo Flash Point, Acabamento revestido de azul e bordas de ouro</p>
                     </div>
                     <div class="buy_product">
-                        <a type="button" href="https://www.google.com/tenisnike">VER MAIS</a>   
+                        <a type="button" href="https://www.google.com/tenisnike">VER MAIS</a>
                     </div>
                 </div>
             </div>
@@ -104,43 +104,92 @@
             <div class="list_info">
 
                 <div class="infoTec">
-                    <div class="infoImg"><img src="./assets/images/info/img.png"/></div>
+                    <div class="infoImg"><img src="./assets/images/info/img.png" /></div>
                     <div class="infoDesc">
-                        <div class="infoTitle"><h1>Fácil e Rápido</h1></div>
+                        <div class="infoTitle">
+                            <h1>Fácil e Rápido</h1>
+                        </div>
                         <div class="infoText"><label>Leva somente poucos segundos para realizar compras na Page Sale.</label></div>
                     </div>
                 </div>
                 <div class="infoTec">
-                    <div class="infoImg"><img src="./assets/images/info/img.png"/></div>
+                    <div class="infoImg"><img src="./assets/images/info/img.png" /></div>
                     <div class="infoDesc">
-                        <div class="infoTitle"><h1>Promoções especiais</h1></div>
+                        <div class="infoTitle">
+                            <h1>Promoções especiais</h1>
+                        </div>
                         <div class="infoText"><label>Não perca nossas ofertas incríveis, sorteios e muito mais somente aqui na Page Sale!</label></div>
                     </div>
                 </div>
 
                 <div class="infoTec">
-                    <div class="infoImg"><img src="./assets/images/info/img.png"/></div>
+                    <div class="infoImg"><img src="./assets/images/info/img.png" /></div>
                     <div class="infoDesc">
-                        <div class="infoTitle"><h1>Pagamentos Convenientes</h1></div>
+                        <div class="infoTitle">
+                            <h1>Pagamentos Convenientes</h1>
+                        </div>
                         <div class="infoText"><label>Pague usando os meios de pagamento mais populares do Brasil</label></div>
                     </div>
                 </div>
 
                 <div class="infoTec">
-                    <div class="infoImg"><img src="./assets/images/info/img.png"/></div>
+                    <div class="infoImg"><img src="./assets/images/info/img.png" /></div>
                     <div class="infoDesc">
-                        <div class="infoTitle"><h1>Suporte Qualificado</h1></div>
+                        <div class="infoTitle">
+                            <h1>Suporte Qualificado</h1>
+                        </div>
                         <div class="infoText"><label>Nossa equipe de suporte está sempre disponível para auxiliar entre 9PM-8AM & 10AM-4PM, 7 dias por semana. </label></div>
                     </div>
                 </div>
-                
+
             </div>
 
         </section>
 
         <footer id="footer">
             <div class="foo_date">
-                
+                <div class="footer_list">
+
+                    <!-- CONTATO -->
+                    <div class="column_footer">
+                        <div class="columnFooterTitle">
+                            <h1>Contato</h1>
+                        </div>
+                        <div class="columnFooterbar"></div>
+                        <div id="contact" class="columnFooterlist">
+                            <div class="list">
+                                <div class="icon_list"><img src="./assets/images/footer/tel.png" alt="Telefone"></div>
+                                <div class="text_list"><h1>+55 69 99292-5520</h1></div>
+                            </div>
+                            <div class="list">
+                                <div class="icon_list"><img src="./assets/images/footer/mail.png" alt="Telefone"></div>
+                                <div class="text_list"><h1>salepage@mail.com</h1></div>
+                            </div>
+                            <div class="list">
+                                <div class="icon_list"><img src="./assets/images/footer/address.png" alt="Telefone"></div>
+                                <div class="text_list"><h1>Rua São Paulo, SP - Setor 2 Quadrado 4</h1></div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- FORMA DE PAGAMENTOS -->
+                    <div class="column_footer">
+                        <div class="columnFooterTitle">
+                            <h1>Forma de Pagamentos</h1>
+                        </div>
+                        <div class="columnFooterbar"></div>
+                        <div id="payment" class="columnFooterlist">
+                                <div class="img_payment"><img src="./assets/images/footer/pay.png" alt="Payment"/></div>
+                        </div>
+                    </div>
+
+
+
+
+
+
+                    
+                </div>
             </div>
         </footer>
     </div>
