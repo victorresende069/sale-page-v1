2e71b0c731af545e337ed3a3bb830e71f044df35
Victor Resende <85452308+victorresende069@users.noreply.github.com>
2022-04-27 07:20:29 -0300

    Delete database.sql

--- app/database/database.sql
+++
@@ -1,6 +0,0 @@
-/*USUÁRIO*/
-CREATE TABLE `ps_v1`.`usuario` ( 
-    `nome` VARCHAR(32) NOT NULL ,  
-`usuario` VARCHAR(16) NOT NULL ,
-  `senha` VARCHAR(64) NOT NULL ,
-    `nivel` INT(2) NOT NULL ) ENGINE = InnoDB;
