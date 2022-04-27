f26fef65c0bd17b9a6b6c8be3149ea1a6e1c3e2e
Victor Resende <xrez.devops@gmail.com>
2022-04-27 07:33:05 -0300

    Update Front & Back

--- app/database/db.php
+++ app/database/db.php
@@ -1,10 +1,10 @@
 <?php
 //BANCO DE DADOS - .GITINGORE
 
-$host_name = '';
-$user_mysql = '';
-$passwd_mysql = '';
-$database_mysql = '';
+$host_name = 'localhost';
+$user_mysql = 'root';
+$passwd_mysql = '12345678';
+$database_mysql = 'ps_v1';
 
 
     $connect_mysql = mysqli_connect($host_name, $user_mysql, $passwd_mysql, $database_mysql) or die (exit(print_r(array('status' => 400))));
